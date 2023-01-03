ed4d5e8ca11e60752fddba3f14ea28d483ea5e5a
dhruvmali254@gmail.com <109059409+Dhruv-Mali@users.noreply.github.com>
2023-01-03 22:40:48 +0530

    opration

---
+++ iforelse.js
@@ -0,0 +1,17 @@
+let a = new Date().getHours
+if(a >= 5 && a <12)
+{
+console.log("good morning")
+}
+else if(a >=12 && a <16)
+{
+console.log("good afternoon")
+}
+else if(a >=16 && a <20)
+{
+console.log("good evening")
+}
+else
+{
+console.log("good night")
+}
